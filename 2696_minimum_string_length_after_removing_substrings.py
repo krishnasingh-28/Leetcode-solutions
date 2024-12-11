69bf302c53c7164cefc7d4edb7adffbcfe5d8c80
krishnasingh-28 <krishnasingh8404@gmail.com>
2024-12-11 07:10:34 +0530

    added

---
+++ 2696_minimum_string_length_after_removing_substrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    # TC --> O(N^2), SC --> O(1)
+    def minLength(self, s: str) -> int:
+        while('AB' in s or 'CD' in s):
+            if 'AB' in s:
+                s = s.replace('AB','')
+            if 'CD' in s:
+                s = s.replace('CD','')
+        return len(s)
