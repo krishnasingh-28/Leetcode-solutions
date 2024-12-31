32459de754c39e5241293a7518a2cc087818f2d1
krishnasingh-28 <krishnasingh8404@gmail.com>
2024-12-31 14:16:22 +0530

    added

---
+++ 43_multiply_strings.py
@@ -0,0 +1,6 @@
+class Solution:
+    # TC --> O(1), SC--> O(1)
+    def multiply(self, num1: str, num2: str) -> str:
+        num1 = int(num1)
+        num2 = int(num2)
+        return str(num1*num2)
