a05382eab552d5af3f40ca8cfe27a4e88a75e61b
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-01-11 22:34:18 +0530

    added

---
+++ 1652_defuse_the_bomb.py
@@ -0,0 +1,13 @@
+class Solution:
+    # TC  --> O(N), SC --> O(N)
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        res = [0]*n
+        for i in range(n):
+            if k > 0:
+                for j in range(i+1, i+1+k):
+                    res[i]  += code[j % n]
+            elif k < 0:
+                for j in range(i-1, i-1-abs(k), -1):
+                    res[i] += code[j % n]
+        return res
