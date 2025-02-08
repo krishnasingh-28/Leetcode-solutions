fd8fcee937ab6b31cb665bc10901321908d5e866
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-02-08 19:38:31 +0530

    added

---
+++ 74_search_in_2D_matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    # Brute Force : TC --> O(M*N), SC --> O(1)
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == target:
+                    return True
+        return False
+    
+    # TC --> O(log(M*N)) optimal approach using binary search, SC --> O(1)
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
