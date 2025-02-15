32e35ae374258cd7dc1091dddf1130523b53d247
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-02-15 21:10:21 +0530

    added

---
+++ 27_remove_element.py
@@ -0,0 +1,9 @@
+class Solution:
+    # TC --> O(N), SC --> O(1)
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
