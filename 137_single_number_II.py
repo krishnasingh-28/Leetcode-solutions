88e47da4aa53730990686b60e6c4bb07c616fcc9
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-03-16 20:26:17 +0530

    added

---
+++ 137_single_number_II.py
@@ -0,0 +1,10 @@
+class Solution:
+    # TC --> O(N), SC --> O(N)
+    def singleNumber(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        for x in nums:
+            count[x] += 1
+        for x, freq in count.items():
+            if freq == 1:
+                return x
+        return -1
