e8d116432780e46e2d48a285e99726372c24e1c7
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-03-26 23:23:28 +0530

    added

---
+++ 1780_check_if_number_is_a_power_of_three.py
@@ -0,0 +1,8 @@
+class Solution:
+    # TC --> O(log3N) SC --> O(1)
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            if n % 3 == 2:
+                return False
+            n = n // 3
+        return True
