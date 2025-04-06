07060b3aadcf7165dcd19fe82ecf25a0604f0f49
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-04-06 23:26:06 +0530

    added

---
+++ 2226_maximum_candies_allocated_to_k_children.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        # TC --> O(N log M), SC --> O(1)
+        total = sum(candies)
+        if total < k:
+            return 0
+        l, r = 1, sum(candies) // k
+        res = 0
+
+        while l <= r:
+            m = (l + r) // 2
+            count = 0
+            for c in candies:
+                if c >= m:
+                    count += c // m
+                if count >= k:
+                    break
+            if count >= k:
+                res = m
+                l = m + 1
+            else:
+                r = m - 1
+        return res
