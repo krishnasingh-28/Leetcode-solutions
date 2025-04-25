23c471e342e080a6f8b2d0f7f3237cadc7557d53
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-04-25 19:20:45 +0530

    added

--- 326_power_of_three.py
+++ 326_power_of_three.py
@@ -1,6 +1,6 @@
 class Solution:
+    # TC --> O(log N), SC --> O(1)
     def isPowerOfThree(self, n: int) -> bool:
-        # TC --> O(log N), SC --> O(1)
         if n <= 0:
             return False
         while n % 3 == 0:
