f81a0758df9db847c870ff39b2d202f91ad7eaaa
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-05-24 20:34:02 +0530

    added

---
+++ 2942_find_words_containing_character.py
@@ -0,0 +1,8 @@
+class Solution:
+    # TC --> O(N*M), SC --> O(N)
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        res = []
+        for i in range(len(words)):
+            if x in words[i]:
+                res.append(i)
+        return res
