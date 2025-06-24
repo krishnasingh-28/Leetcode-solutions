790ca6b7ff21023d4a2f1d006dba4f3487ffad1f
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-06-25 00:18:00 +0530

    added

---
+++ 2200_find_all_k-distant_indices_in_an_array.py
@@ -0,0 +1,11 @@
+class Solution:
+    # TC --> O(N^2), SC --> O(1)
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        n = len(nums)
+        res = []
+        for i in range(n):
+            for j in range(n):
+                if nums[j] == key and abs(i - j) <= k:
+                    res.append(i)
+                    break
+        return res
