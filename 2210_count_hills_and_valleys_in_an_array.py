766b6838140846036e7264c3084c26ed33c38fd6
krishnasingh-28 <krishnasingh8404@gmail.com>
2025-07-27 23:11:33 +0530

    added

---
+++ 2210_count_hills_and_valleys_in_an_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    # TC --> O(N), SC --> O(1)
+    def countHillValley(self, nums: List[int]) -> int:
+        count = 0
+        left = 0
+
+        for i in range(1, len(nums) - 1):
+            if nums[i] != nums[i + 1]:
+                if (nums[i] > nums[left] and nums[i] > nums[i + 1]) or (
+                    nums[i] < nums[left] and nums[i] < nums[i + 1]
+                ):
+                    count += 1
+                left = i
+
+        return count
